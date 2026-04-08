45c76ccbeba59c50bc5729aab58423293ce52b36
David Ndung'u <daviexdee170@gmail.com>
2026-04-08 21:13:46 +0300

    First CPP

---
+++ Manane.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main(){
+    std::cout << "I woke up to write some code" <<'\n';
+    std::cout << "Or rather I couldn't sleep and thought...";
+    std::cout << "it was wise to write something that i learned the previous day";
+
+return 0;
+}
---
+++ myne.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main() {
+    std::cout <<"I like playing the piano." <<'\n';
+    std::cout <<"Can I say I like Maritim again?" <<'\n';
+    std::cout <<"Now I'm going to attend the Navs Bible Study at Susan and Ann's place behind Juja market." <<'\n';
+    std::cout <<"Adios Muchachos...PEACE!!!";
+
+    return 0;
+}
